47bb2d5ed5d6585283ea4f66658b271fcb55b72b
LaszloOvari <laszlo.ovari@ymail.com>
2020-01-08 14:50:15 +0100

    Add project files.

---
+++ FrogRiverOne/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogRiverOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 1 };
+            Console.WriteLine(Solution(2, A));
+        }
+
+        public static int Solution(int X, int[] A)
+        {
+            int[] B = new int[X];
+            int leaves = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (B[A[i] - 1] == 0)
+                {
+                    B[A[i] - 1]++;
+                    leaves++;
+                    if (leaves == X) return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
